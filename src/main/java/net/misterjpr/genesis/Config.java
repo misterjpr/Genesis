2e02db40720ef68dee177a0e7d10e6e5402a6136
jprap <jprappendino@gmail.com>
2025-05-09 09:29:23 -0300

    Genesis config file

--- src/main/java/net/misterjpr/genesis/Config.java
+++ src/main/java/net/misterjpr/genesis/Config.java
@@ -1,7 +1,5 @@
 package net.misterjpr.genesis;
 
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.config.ModConfigEvent;
@@ -11,20 +9,9 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
 
-    private static final ModConfigSpec.BooleanValue MOD_LOG = BUILDER
-            .comment("The mod Log")
-            .define("modlog", true);
-
     static final ModConfigSpec SPEC = BUILDER.build();
-
-    public static boolean modLog;
-
-    private static boolean validateItemName(final Object obj) {
-        return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(itemName));
-    }
 
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event) {
-        modLog = MOD_LOG.get();
     }
 }
